84e5a334205da43dae92221462c483e9f8c72779
LG556 <85017507+LG556@users.noreply.github.com>
2024-11-18 17:58:22 +0200

    тепер всі верхні if мають умову назви, а під ними немає умов назви можемо умовно скоротити їх в 3 if що відповідають за оновлення одного типу предмету (важливо зважати на порядок , якщо умова вище за ту в яку ми маємо її інтегрувати то ми маємо вставити її вище , якщо нижче то нижче ітд.

--- src/main/java/com/Lab3/Main.java
+++ src/main/java/com/Lab3/Main.java
@@ -13,6 +13,12 @@ class Main {
                 if (items[i].quality > 0) {
                     items[i].quality -= 1;
                 }
+                items[i].sellIn -= 1;
+                if (items[i].sellIn < 0) {
+                    if (items[i].quality > 0) {
+                        items[i].quality -= 1;
+                    }
+                }
             }
             if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                 if (items[i].sellIn < 11) {
@@ -21,42 +27,27 @@ class Main {
                 if (items[i].sellIn < 6) {
                     items[i].quality += 1;
                 }
-            }
-
-
-
-            if (!items[i].name.equals("Normal Item")) {
+                items[i].sellIn -= 1;
                 if (items[i].quality < 50) {
                     items[i].quality += 1;
                 }
+                if (items[i].sellIn < 0) {
+                    items[i].quality = 0;
+                }
             }
 
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                items[i].sellIn -= 1;
-            }
 
             if (items[i].name.equals("Aged Brie")) {
+                if (items[i].quality < 50) {
+                    items[i].quality += 1;
+                }
                 if (items[i].sellIn < 0) {
                     if (items[i].quality < 50) {
                         items[i].quality += 1;
                     }
                 }
+                items[i].sellIn -= 1;
             }
-            if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (items[i].sellIn < 0) {
-                    items[i].quality = 0;
-                }
-            }
-            if (items[i].name.equals("Normal Item")) {
-                if (items[i].sellIn < 0) {
-                    if (items[i].quality > 0) {
-                        items[i].quality -= 1;
-                    }
-                }
-            }
-
-
-
 
 
 
