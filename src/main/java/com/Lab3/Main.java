be68d3ab149fcb64a80a9c0d11f3b489b4b35eb7
LG556 <85017507+LG556@users.noreply.github.com>
2024-11-18 12:41:11 +0200

    умова зо предмет не є особливим -> умова що предмет звичайний

--- src/main/java/com/Lab3/Main.java
+++ src/main/java/com/Lab3/Main.java
@@ -9,16 +9,13 @@ class Main {
 
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")
-                    && !items[i].name.equals("Sulfuras, Hand of Ragnaros")
+            if (items[i].name.equals("Normal Item")
                     && items[i].quality > 0) {
                 items[i].quality -=1;
 
             } else {
                 if (items[i].quality < 50) {
                     items[i].quality +=1 ;
-
                     if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                         if (items[i].sellIn < 11) {
                             items[i].quality += 1;
