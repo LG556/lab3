0cd5d09b72ab37ea9e9d686d52115ece1abdaafa
LG556 <85017507+LG556@users.noreply.github.com>
2024-11-18 18:35:44 +0200

    виносимо в окремі методи логіку кожного з 3 if з методу updateQuality()

--- src/main/java/com/Lab3/Main.java
+++ src/main/java/com/Lab3/Main.java
@@ -9,48 +9,50 @@ class Main {
 
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
-            if (items[i].name.equals("Normal Item") ) {
-                if (items[i].quality > 0) {
-                    items[i].quality -= 1;
-                }
-                items[i].sellIn -= 1;
-                if (items[i].sellIn < 0) {
-                    if (items[i].quality > 0) {
-                        items[i].quality -= 1;
-                    }
-                }
-            }
-            if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (items[i].sellIn < 11) {
-                    items[i].quality += 1;
-                }
-                if (items[i].sellIn < 6) {
-                    items[i].quality += 1;
-                }
-                items[i].sellIn -= 1;
-                if (items[i].quality < 50) {
-                    items[i].quality += 1;
-                }
-                if (items[i].sellIn < 0) {
-                    items[i].quality = 0;
-                }
+            if (items[i].name.equals("Normal Item")) {
+                updateNormalItem(items[i]);
+            } else if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                updateBackstagePass(items[i]);
+            } else if (items[i].name.equals("Aged Brie")) {
+                updateAgedBrie(items[i]);
             }
 
+        }
+    }
 
-            if (items[i].name.equals("Aged Brie")) {
-                if (items[i].quality < 50) {
-                    items[i].quality += 1;
-                }
-                if (items[i].sellIn < 0) {
-                    if (items[i].quality < 50) {
-                        items[i].quality += 1;
-                    }
-                }
-                items[i].sellIn -= 1;
-            }
+    private void updateNormalItem(Item item) {
+        if (item.quality > 0) {
+            item.quality -= 1;
+        }
+        item.sellIn -= 1;
+        if (item.sellIn < 0 && item.quality > 0) {
+            item.quality -= 1;
+        }
+    }
 
+    private void updateBackstagePass(Item item) {
+        if (item.sellIn < 11 && item.quality < 50) {
+            item.quality += 1;
+        }
+        if (item.sellIn < 6 && item.quality < 50) {
+            item.quality += 1;
+        }
+        if (item.quality < 50) {
+            item.quality += 1;
+        }
+        item.sellIn -= 1;
+        if (item.sellIn < 0) {
+            item.quality = 0;
+        }
+    }
 
-
+    private void updateAgedBrie(Item item) {
+        if (item.quality < 50) {
+            item.quality += 1;
+        }
+        item.sellIn -= 1;
+        if (item.sellIn < 0 && item.quality < 50) {
+            item.quality += 1;
         }
     }
 }
