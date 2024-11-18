322eb35fe5ebe1a3edf04edb277e5b2c0b59ea10
LG556 <85017507+LG556@users.noreply.github.com>
2024-11-18 12:16:10 +0200

    вкладені if які перевіряють умови замінив на один з тим самим функціоналом

--- src/main/java/com/Lab3/Main.java
+++ src/main/java/com/Lab3/Main.java
@@ -9,13 +9,9 @@ class Main {
 
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                        items[i].quality -=1;
-                    }
-                }
+            if (!items[i].name.equals("Aged Brie") && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert") && items[i].quality > 0 && !items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+                items[i].quality -=1;
+
             } else {
                 if (items[i].quality < 50) {
                     items[i].quality +=1 ;
