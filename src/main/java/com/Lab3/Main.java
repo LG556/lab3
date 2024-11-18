5a31914df217215a35cf8e1c2ccaa43a39ad66c3
LG556 <85017507+LG556@users.noreply.github.com>
2024-11-18 17:38:59 +0200

    йдемо до того щоб розділити оновлення предметів на окремі методи, відкриваємо загальний if на окремі з тоюж логікою

--- src/main/java/com/Lab3/Main.java
+++ src/main/java/com/Lab3/Main.java
@@ -35,23 +35,31 @@ class Main {
                 items[i].sellIn -= 1;
             }
 
-            if (items[i].sellIn < 0) {
-                if (items[i].name.equals("Aged Brie")) {
+            if (items[i].name.equals("Aged Brie")) {
+                if (items[i].sellIn < 0) {
                     if (items[i].quality < 50) {
                         items[i].quality += 1;
                     }
                 }
-                if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+            }
+            if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                if (items[i].sellIn < 0) {
                     items[i].quality = 0;
                 }
-
-
-                if (items[i].name.equals("Normal Item")) {
-                    if (items[i].quality > 0 ) {
+            }
+            if (items[i].name.equals("Normal Item")) {
+                if (items[i].sellIn < 0) {
+                    if (items[i].quality > 0) {
                         items[i].quality -= 1;
                     }
                 }
             }
+
+
+
+
+
+
         }
     }
 }
