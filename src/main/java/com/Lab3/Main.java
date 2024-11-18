e14adef974030790647d463a16a278289b90c4b5
LG556 <85017507+LG556@users.noreply.github.com>
2024-11-18 12:21:42 +0200

    ще один вкладений if

--- src/main/java/com/Lab3/Main.java
+++ src/main/java/com/Lab3/Main.java
@@ -39,10 +39,10 @@ class Main {
             if (items[i].sellIn < 0) {
                 if (!items[i].name.equals("Aged Brie")) {
                     if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                                items[i].quality -= 1;
-                            }
+                        if (items[i].quality > 0 &&!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+                            items[i].quality -= 1;
+
+
                         }
                     } else {
                         items[i].quality = 0;
